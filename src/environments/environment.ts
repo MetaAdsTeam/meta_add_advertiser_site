1e545be6d73df1b22103714ee8749d5dc72fb719
trenger <posadsky@gmail.com>
2022-02-09 21:13:05 +0300

    changed nft-contract

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,9 +9,9 @@ export const environment = {
     walletUrl: "https://wallet.testnet.near.org",
     helperUrl: "https://helper.testnet.near.org",
     explorerUrl: "https://explorer.testnet.near.org",
-    contractId: 'nfttest.evgeniy_test.testnet',
+    contractId: 'contract.nft_evgeniy_test.testnet',
     app: 'MetaAds',
-    accountId: 'evgeniy_test.testnet'
+    accountId: 'nft_evgeniy_test.testnet'
 },
   devmode: true
 };
