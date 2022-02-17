ed570498a011d422177328615815a431e3663fa2
Alexander <alexander.do@wecandevelopit.com>
2022-02-17 13:45:46 +0300

    Fix timeslots date format

--- src/app/pipes/date-format.pipe.ts
+++ src/app/pipes/date-format.pipe.ts
@@ -8,10 +8,7 @@ export class DateFormatPipe implements PipeTransform {
       if (type === 'DDMMMM') {
         return date.toLocaleString({day: 'numeric', month: 'long'});
       } else if (type === 'HH12MM') {
-        return date
-          .toLocaleString({hour: 'numeric', hour12: true, minute: '2-digit'})
-          .split(' ')[0]
-          .replace(':', '.')
+        return date.toFormat('hh:mm')
       } else {
         return ''
       }
